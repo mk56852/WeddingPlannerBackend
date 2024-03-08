c2a310f064f0b8d389ed7ebbef0371bdf962b591
DESKTOP-UJD65FI\dell <melek7967@gmail.com>
2024-03-08 21:29:19 +0100

    initilize project

---
+++ src/main/java/com/example/WeddingPlannerServer/WeddingPlannerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeddingPlannerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingPlannerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingPlannerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WeddingPlannerServer/WeddingPlannerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WeddingPlannerServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingPlannerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
